ec6fa6380814181377af25e7b05ea590a4f4da9e
mae916 <ruchia916@naver.com>
2021-10-27 23:51:40 +0900

    vue 학습

---
+++ vue3/vue-project/src/views/DataBindingCheckbox.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <label>
+            <input type="checkbox" v-model="checked" true-value="yes" false-value="no"> {{ checked }}
+        </label>
+
+        <br><br>
+        <label>
+            <input type="checkbox" v-bind:value="checkValue" v-model="checked2"> {{ checkValue }}
+        </label>
+    </div> 
+</template>
+<script>
+export default {
+    data() {
+        return {
+            checked : "yes",
+            checked2 : true,
+            checkValue : "체크박스 값",
+        };
+    }
+}
+</script>
