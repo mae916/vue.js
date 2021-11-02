c4107d8ba3e41206db085185619927f8b490f335
mae916 <ruchia916@naver.com>
2021-11-02 22:58:01 +0900

    vue 학습

---
+++ vue3/vue-project2/src/views/DataBinding2.vue
@@ -0,0 +1,37 @@
+<template>
+    <input type="number" v-model.number="num1" /> + 
+    <input type="number" v-model.number="num2" /> = 
+    {{ result }} 
+    <br><br>
+    <button type="button" @click="msg">클릭!</button>
+</template>
+<script>
+import { onMounted, onUnmounted } from 'vue';
+import calculator from "../calculator.js";
+export default {
+    created() {
+        console.log("Created!");
+    },
+    setup() {
+        function msg() {
+            alert("메세지 테스트!");
+        }
+        
+        onMounted(() => {
+            console.log("Mounted!");
+        });
+
+        onUnmounted(() => {
+            console.log("Unmounted");
+        });
+
+        let { num1, num2, result } = calculator();
+        return {
+            num1,
+            num2,
+            result,
+            msg,
+        }
+    }
+}
+</script>
---
+++ vue3/vue-project2/src/views/DataBinding4.vue
@@ -0,0 +1,43 @@
+<template>
+    <textarea v-model="message"></textarea>
+    <br>
+
+    <select v-model="city">
+        <option value="02">서울</option>
+        <option value="032">인천</option>
+        <option value="064">제주</option>
+    </select>
+    <br>
+    <label>
+        <input type="checkbox" v-model="checked" true-value="yes" false-value="no">{{ checked }}
+    </label>
+    <br><br>
+    <h1>좋아하는 과일은?</h1>
+    <label>
+        <input type="checkbox" v-model="fruits" value="Apple">Apple
+    </label>
+    <label>
+        <input type="checkbox" v-model="fruits" value="Orange">Orange
+    </label>
+    <label>
+        <input type="checkbox" v-model="fruits" value="Melon">Melon
+    </label>
+    <button type="button" @click="checkValue">확인</button>
+</template>
+<script> 
+export default {
+    data() {
+        return {
+            fruits : [],
+            checked : "yes", // true, no -> false
+            message : "textarea에 입력될 문장....",
+            city : "032",
+        };
+    },
+    methods : {
+        checkValue() {
+            console.log(this.fruits);
+        }
+    }
+}
+</script>
